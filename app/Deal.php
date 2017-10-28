eb2916e47c39342989c7ee706f6a962c45cf9bd6
lyckan <callejasmartinez12@gmail.com>
2017-10-27 18:29:59 -0600

    Agrego modelos

--- app/Deal.php
+++ app/Deal.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Deal extends Model
 {
-    //
+    protected $fillable = [
+        'product_id',
+        'title',
+        'subtitle',
+        'image_url',
+        'image_path',
+        'description',
+        'likes'
+    ];
 }
--- app/Products.php
+++ app/Products.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Products extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'picture_url',
+        'picture_path',
+        'manufacturer_id',
+        'likes',
+        'searches'
+    ];
 }
