379f939b857065c602ca0803140a7e4f296e10e4
lyckan <callejasmartinez12@gmail.com>
2017-10-27 23:37:08 -0600

    Agrego nuevos elementos

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('/dev', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
