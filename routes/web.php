49d677cec91b7c4f56a563fd740ac79d74ebfc90
lyckan <callejasmartinez12@gmail.com>
2017-10-30 12:22:43 -0600

    agrego view add product

--- routes/web.php
+++ routes/web.php
@@ -15,87 +15,82 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dev', function () {
-    return view('home');
-});
-
-
 // Products
 Route::get('/products/add', function () {
     return view('/products/add');
-});
+})->middleware('auth');
 
 Route::get('/products/edit', function () {
     return view('/products/edit');
-});
+})->middleware('auth');
 
 Route::get('/products/show', function () {
     return view('/products/show');
-});
+})->middleware('auth');
 
 Route::get('/products/update', function () {
     return view('/products/update');
-});
+})->middleware('auth');
 
 Route::get('/products/add-complete', function () {
     return view('/products/return-add');
-});
+})->middleware('auth');
 
 Route::get('/products/update-complete', function () {
     return view('/products/return-update');
-});
+})->middleware('auth');
 
 // Deals
 
 Route::get('/deals/add', function () {
     return view('/deals/add');
-});
+})->middleware('auth');
 
 Route::get('/deals/edit', function () {
     return view('/deals/edit');
-});
+})->middleware('auth');
 
 Route::get('/deals/show', function () {
     return view('/deals/show');
-});
+})->middleware('auth');
 
 Route::get('/deals/update', function () {
     return view('/deals/update');
-});
+})->middleware('auth');
 
 Route::get('/deals/add-complete', function () {
     return view('/deals/return-add');
-});
+})->middleware('auth');
 
 Route::get('/deals/update-complete', function () {
     return view('/deals/return-update');
-});
+})->middleware('auth');
 
 // Users
 
 Route::get('/users/add', function () {
     return view('/users/add');
-});
+})->middleware('auth');
 
 Route::get('/users/edit', function () {
     return view('/users/edit');
-});
+})->middleware('auth');
 
 Route::get('/users/show', function () {
     return view('/users/show');
-});
+})->middleware('auth');
 
 Route::get('/users/update', function () {
     return view('/users/update');
-});
+})->middleware('auth');
 
 Route::get('/users/add-complete', function () {
     return view('/users/return-add');
-});
+})->middleware('auth');
 
 Route::get('/users/update-complete', function () {
     return view('/users/return-update');
-});
+})->middleware('auth');
 
 Auth::routes();
 
